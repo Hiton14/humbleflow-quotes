4906cb25e0fa1b2dfedbb443a3a7394032edcdc6
Hiton14 <hitonmwaks14@gmail.com>
2026-01-25 16:31:20 +0300

    feat: Implement sitemap generation using `vite-plugin-sitemap` in Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +14,15 @@ export default defineConfig(({ mode }) => ({
     outDir: 'dist',
     emptyOutDir: true,
   },
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://www.humblebosstechnical.co.ke',
+      dynamicRoutes: [
+        '/products',
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
